4e7e46b69b9476369e0cdc2822318b5bcd617524
Sruti <srutis90@gmail.com>
2014-12-17 14:18:14 -0800

    TEMP - attempt to parallelize with 1D ; doesn't work

--- utils/js/benchmark.js
+++ utils/js/benchmark.js
@@ -3,7 +3,7 @@
 //}
 //
 //for(var i=0; i<5; i++)
-//    runPageRank();
+    runPageRank(10, 10);
 
 //spmvRun(dim, density, stddev, iterations)
 
@@ -13,6 +13,4 @@
 
 var pa = new ParallelArray([1,2,3]);
 var cubes = pa.map(function(x){return x*x*x;});
-console.log(cubes.data[0]);
-console.log(cubes.data[1]);
 console.log(cubes.data[2]);
