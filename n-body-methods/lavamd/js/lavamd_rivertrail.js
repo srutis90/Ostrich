e5c5014e5a1306dc022a122904455b7b0361405d
Sruti <srutis90@gmail.com>
2014-12-18 14:24:26 -0800

    cponvert everything to array of arrays

--- n-body-methods/lavamd/js/lavamd_rivertrail.js
+++ n-body-methods/lavamd/js/lavamd_rivertrail.js
@@ -6,9 +6,9 @@ if (typeof performance === "undefined") {
 var NUMBER_PAR_PER_BOX = 100;
 
 function DOT(A,B) {
-    return ((A.data[0])*(B.data[0])
-            +(A.data[1])*(B.data[1])
-            +(A.data[2])*(B.data[2]));
+    return ((A[0])*(B[0])
+            +(A[1])*(B[1])
+            +(A[2])*(B[2]));
 }
 
 function createArray(creator, size) {
@@ -28,20 +28,13 @@ function nei_str() {
 
 function box_str() {
     // home box
-    return {
-
-        //x,y,x,number, offset, nn
-        data: new Float32Array([0,0,0,0,0,0]),
-        // neighbor boxes
-        neighbours: createArray(nei_str, 26)
-    };
+    //x,y,x,number, offset, nn
+    return new Float32Array([0,0,0,0,0,0]);
 }
 
 function space_mem() {
-    return {
-        //x,y,z,v
-        data:new Float32Array([0,0,0,0])
-    }
+    //x,y,z,v
+    return new Float32Array([0,0,0,0]);
 }
 
 function lavamd(boxes1d) {
@@ -51,7 +44,9 @@ function lavamd(boxes1d) {
     var i, j, k, l, m, n, expected_boxes1d = 6;
 
     // system memory
-    var par_cpu = {}, dim_cpu = {}, box_cpu = [], rv_cpu = [], qv_cpu, fv_cpu = [], nh;
+    var par_cpu = {}, dim_cpu = {};
+    var box_cpu = [], rv_cpu = [], qv_cpu, fv_cpu = [], nh;
+    var neighbors = [];
     var expectedAns = [4144561.0, 181665.0, -190914.0, 140373.0];
 
     // assign default values
@@ -76,6 +71,7 @@ function lavamd(boxes1d) {
 
     // BOX
     box_cpu = createArray(box_str, dim_cpu.number_boxes);   // allocate boxes
+    neighbors = createArray(nei_str, dim_cpu.number_boxes * 26); //allocate neighbors for each box
     // initialize number of home boxes
     nh = 0;
 
@@ -88,14 +84,14 @@ function lavamd(boxes1d) {
 
                 // current home box
                 //[x, y, x, number, offset, nn]
-                box_cpu[nh].data[0] = k;
-                box_cpu[nh].data[1] = j;
-                box_cpu[nh].data[2] = i;
-                box_cpu[nh].data[3] = nh;
-                box_cpu[nh].data[4] = nh * NUMBER_PAR_PER_BOX;
+                box_cpu[nh][0] = k;
+                box_cpu[nh][1] = j;
+                box_cpu[nh][2] = i;
+                box_cpu[nh][3] = nh;
+                box_cpu[nh][4] = nh * NUMBER_PAR_PER_BOX;
 
                 // initialize number of neighbor boxes
-                box_cpu[nh].data[5] = 0;
+                box_cpu[nh][5] = 0;
 
                 // neighbor boxes in z direction
                 for(l=-1; l<2; l++){
@@ -108,18 +104,20 @@ function lavamd(boxes1d) {
                                     (l==0 && m==0 && n==0)==false){
 
                                 // current neighbor box
-                                var nn = box_cpu[nh].data[5];
-                                box_cpu[nh].neighbours[nn][0] = (k+n);
-                                box_cpu[nh].neighbours[nn][1] = (j+m);
-                                box_cpu[nh].neighbours[nn][2] = (i+l);
-                                box_cpu[nh].neighbours[nn][3] =
-                                    (box_cpu[nh].neighbours[nn][2] * dim_cpu.boxes1d_arg * dim_cpu.boxes1d_arg) +
-                                    (box_cpu[nh].neighbours[nn][1] * dim_cpu.boxes1d_arg) +
-                                    box_cpu[nh].neighbours[nn][0];
-                                box_cpu[nh].neighbours[nn][4] = box_cpu[nh].neighbours[nn][3] * NUMBER_PAR_PER_BOX;
+                                var nn = box_cpu[nh][5];
+                                var neighborIndex = nh * 26 + nn;
+                                var currentNeighbor = neighbors[neighborIndex];
+                                currentNeighbor[0] = (k+n);
+                                currentNeighbor[1] = (j+m);
+                                currentNeighbor[2] = (i+l);
+                                currentNeighbor[3] =
+                                    (currentNeighbor[2] * dim_cpu.boxes1d_arg * dim_cpu.boxes1d_arg) +
+                                    (currentNeighbor[1] * dim_cpu.boxes1d_arg) +
+                                    currentNeighbor[0];
+                                currentNeighbor[4] = currentNeighbor[3] * NUMBER_PAR_PER_BOX;
 
                                 // increment neighbor box
-                                box_cpu[nh].data[5] = nn + 1;
+                                box_cpu[nh][5] = nn + 1;
                             }
                         } // neighbor boxes in x direction
                     } // neighbor boxes in y direction
@@ -134,10 +132,10 @@ function lavamd(boxes1d) {
     // input (distances)
     rv_cpu = createArray(space_mem, dim_cpu.space_elem); //(FOUR_VECTOR*)malloc(dim_cpu.space_mem);
     for(i=0; i<dim_cpu.space_elem; i=i+1){
-        rv_cpu[i].data[3] = (Math.commonRandom()%10 + 1) / 10.0;        // get a number in the range 0.1 - 1.0
-        rv_cpu[i].data[0] = (Math.commonRandom()%10 + 1) / 10.0;        // get a number in the range 0.1 - 1.0
-        rv_cpu[i].data[1] = (Math.commonRandom()%10 + 1) / 10.0;        // get a number in the range 0.1 - 1.0
-        rv_cpu[i].data[2] = (Math.commonRandom()%10 + 1) / 10.0;        // get a number in the range 0.1 - 1.0
+        rv_cpu[i][3] = (Math.commonRandom()%10 + 1) / 10.0;        // get a number in the range 0.1 - 1.0
+        rv_cpu[i][0] = (Math.commonRandom()%10 + 1) / 10.0;        // get a number in the range 0.1 - 1.0
+        rv_cpu[i][1] = (Math.commonRandom()%10 + 1) / 10.0;        // get a number in the range 0.1 - 1.0
+        rv_cpu[i][2] = (Math.commonRandom()%10 + 1) / 10.0;        // get a number in the range 0.1 - 1.0
     }
 
     // input (charge)
@@ -152,22 +150,22 @@ function lavamd(boxes1d) {
     console.log("Rivertrail enabled");
     time0 = performance.now();
 
-    kernel_cpu(par_cpu, dim_cpu, box_cpu, rv_cpu, qv_cpu, fv_cpu);
+    kernel_cpu(par_cpu, dim_cpu, box_cpu, rv_cpu, qv_cpu, fv_cpu, neighbors);
 
     var sum = space_mem();
     if (dim_cpu.boxes1d_arg == expected_boxes1d) {
         for(i=0; i<dim_cpu.space_elem; i=i+1) {
-            sum.data[3] += fv_cpu[i].data[3];
-            sum.data[0] += fv_cpu[i].data[0];
-            sum.data[1] += fv_cpu[i].data[1];
-            sum.data[2] += fv_cpu[i].data[2];
+            sum[3] += fv_cpu[i][3];
+            sum[0] += fv_cpu[i][0];
+            sum[1] += fv_cpu[i][1];
+            sum[2] += fv_cpu[i][2];
         }
-        if(Math.round(sum.data[3]) != expectedAns[0] ||
-            Math.round(sum.data[0]) != expectedAns[1] ||
-            Math.round(sum.data[1]) != expectedAns[2] ||
-            Math.round(sum.data[2]) != expectedAns[3]) {
+        if(Math.round(sum[3]) != expectedAns[0] ||
+            Math.round(sum[0]) != expectedAns[1] ||
+            Math.round(sum[1]) != expectedAns[2] ||
+            Math.round(sum[2]) != expectedAns[3]) {
             console.log("Expected: [" + expectedAns[0] + ", " + expectedAns[1] + ", " + expectedAns[2] + ", " + expectedAns[3] + "]");
-            console.log("Got: [" + sum.data[3] + ", " + sum.data[0] + ", " + sum.data[1] + ", " + sum.data[2] + "]");
+            console.log("Got: [" + sum[3] + ", " + sum[0] + ", " + sum[1] + ", " + sum[2] + "]");
         }
     } else {
         console.log("WARNING: no self-checking for input size of '%d'\n", dim_cpu.boxes1d_arg);
@@ -180,7 +178,7 @@ function lavamd(boxes1d) {
              time: (time1 - time0) / 1000 };
 }
 
-function kernel_cpu(par, dim, box, rv, qv, fv) {
+function kernel_cpu(par, dim, box, rv, qv, fv, nei) {
     var alpha, a2;                      // parameters
     var i, j, k, l;                     // counters
     var first_i, pointer, first_j;      // neighbor box
@@ -195,37 +193,38 @@ function kernel_cpu(par, dim, box, rv, qv, fv) {
 
     for(l=0; l<dim.number_boxes; l=l+1) {
         // home box - box parameters
-        first_i = box[l].data[4];
+        first_i = box[l][4];
 
         //  Do for the # of (home+neighbor) boxes
-        for(k=0; k<(1+box[l].data[5]); k++) {
+        for(k=0; k<(1+box[l][5]); k++) {
             //  neighbor box - get pointer to the right box
             if(k==0) {
                 pointer = l;    // set first box to be processed to home box
             } else {
-                pointer = box[l].neighbours[k-1][3];   // remaining boxes are neighbor boxes
+                var boxNeighborStart = l * 26;
+                pointer = nei[boxNeighborStart + k-1][3];   // remaining boxes are neighbor boxes
             }
 
-            first_j = box[pointer].data[4];
+            first_j = box[pointer][4];
 
             for(i=0; i<NUMBER_PAR_PER_BOX; i=i+1) {
                 for(j=0; j<NUMBER_PAR_PER_BOX; j=j+1) {
-                    r2 = rv[first_i+i].data[3] + rv[first_j+j].data[3] - DOT(rv[first_i+i],rv[first_j+j]);
+                    r2 = rv[first_i+i][3] + rv[first_j+j][3] - DOT(rv[first_i+i],rv[first_j+j]);
                     u2 = a2*r2;
                     vij= Math.exp(-u2);
                     fs = 2.*vij;
-                    var dx = rv[first_i+i].data[0]  - rv[first_j+j].data[0];
-                    var dy = rv[first_i+i].data[1]  - rv[first_j+j].data[1];
-                    var dz = rv[first_i+i].data[2]  - rv[first_j+j].data[2];
+                    var dx = rv[first_i+i][0]  - rv[first_j+j][0];
+                    var dy = rv[first_i+i][1]  - rv[first_j+j][1];
+                    var dz = rv[first_i+i][2]  - rv[first_j+j][2];
                     fxij=fs*dx;
                     fyij=fs*dy;
                     fzij=fs*dz;
 
                     // forces [x,y,z,v]
-                    fv[first_i+i].data[3] +=  qv[first_j+j]*vij;
-                    fv[first_i+i].data[0] +=  qv[first_j+j]*fxij;
-                    fv[first_i+i].data[1] +=  qv[first_j+j]*fyij;
-                    fv[first_i+i].data[2] +=  qv[first_j+j]*fzij;
+                    fv[first_i+i][3] +=  qv[first_j+j]*vij;
+                    fv[first_i+i][0] +=  qv[first_j+j]*fxij;
+                    fv[first_i+i][1] +=  qv[first_j+j]*fyij;
+                    fv[first_i+i][2] +=  qv[first_j+j]*fzij;
                 }
             }
         }
