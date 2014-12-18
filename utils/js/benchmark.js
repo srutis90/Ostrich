3740abeafed1810cf3cae94d9421f76329e8e274
Sruti <srutis90@gmail.com>
2014-12-17 22:49:52 -0800

    refactored to rivertrail

--- utils/js/benchmark.js
+++ utils/js/benchmark.js
@@ -3,14 +3,9 @@
 //}
 //
 //for(var i=0; i<5; i++)
-    runPageRank(10, 10);
+    runPageRank();
 
 //spmvRun(dim, density, stddev, iterations)
 
 //for(var i=0; i<5;i++)
 //    spmvRun(50000, 2000, 0.01, 5);
-
-
-var pa = new ParallelArray([1,2,3]);
-var cubes = pa.map(function(x){return x*x*x;});
-console.log(cubes.data[2]);
